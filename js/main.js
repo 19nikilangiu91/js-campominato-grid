e3f5dd50db75c19842c27b71bb5f8829344256fc
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-17 13:31:47 +0100

    Upgrade Consegna

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Consegna:
+// 1) Consegna:
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
